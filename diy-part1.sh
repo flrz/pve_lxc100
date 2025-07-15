d042b3db5c6a69a3163f5d03a4d5b7da0bb4dbec
lieet <59359907+flrz@users.noreply.github.com>
2025-07-15 21:43:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,11 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
-sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
+#sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 #sed -i '1i src-git passwall_packages https://github.com/ffff3333/packages' feeds.conf.default
-#sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '2i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
+sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '2i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 #sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '3i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
 sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
