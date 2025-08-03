1ad178d6dbe8d2fcd5814f0b05f528dbf2013fdb
lieet <59359907+flrz@users.noreply.github.com>
2025-08-03 21:53:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,14 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
-src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main
-src-git packages https://git.openwrt.org/feed/packages.git;openwrt-24.10
-src-git luci https://git.openwrt.org/project/luci.git;openwrt-24.10
-src-git routing https://git.openwrt.org/feed/routing.git;openwrt-24.10
-src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-24.10
-src-git argon https://github.com/ffff3333/themes.git;custom
-src-git openclash https://github.com/vernesong/OpenClash.git;custom
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
@@ -32,5 +24,5 @@ src-git openclash https://github.com/vernesong/OpenClash.git;custom
 #sed -i '1i src-git rzpackages https://github.com/flrz/rzpackages' feeds.conf.default
 #sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 #sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '3i src-git themes https://github.com/ffff3333/themes' feeds.conf.default
+sed -i '3i src-git argon https://github.com/ffff3333/themes' feeds.conf.default
 #sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
