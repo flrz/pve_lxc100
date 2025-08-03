a562916d48574a72ec565bebf0568a9421dc154a
lieet <59359907+flrz@users.noreply.github.com>
2025-08-03 09:32:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@
 #echo 'src-git themes https://github.com/ffff3333/themes' >>feeds.conf.default
 #sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
 #sed -i '1i src-git diypackages https://github.com/flrz/diypackages' feeds.conf.default
-#sed -i '1i src-git rzpackages https://github.com/flrz/rzpackages' feeds.conf.default
-sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '1i src-git rzpackages https://github.com/flrz/rzpackages' feeds.conf.default
+#sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '3i src-git themes https://github.com/ffff3333/themes' feeds.conf.default
 sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
