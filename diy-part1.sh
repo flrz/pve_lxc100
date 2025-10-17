ee4e98f04f8dba8b1c018273ed4fa461f9633bb8
lieet <59359907+flrz@users.noreply.github.com>
2025-10-17 21:47:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,13 @@
 # Uncomment a feed source
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-#https://github.com/xiaorouji/openwrt-passwall/discussions/1603
-
+# 移除 openwrt feeds 自带的核心库
+#cd openwrt
+#rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# 移除 openwrt feeds 过时的luci版本
+#rm -rf feeds/luci/applications/luci-app-passwall
+#it clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
@@ -24,7 +29,7 @@
 #sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
 #sed -i '1i src-git diypackages https://github.com/flrz/diypackages' feeds.conf.default
 #sed -i '1i src-git rzpackages https://github.com/flrz/rzpackages' feeds.conf.default
-#sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-#sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '1i src-git argon https://github.com/flrz/themes' feeds.conf.default
-sed -i '2i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '1i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '3i src-git argon https://github.com/flrz/themes' feeds.conf.default
+sed -i '4i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
